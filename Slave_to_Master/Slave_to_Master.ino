2e53e984c916e431f8e8e071c9fdb2bb9c988d8c
JunhyungPark1821 <50926511+JunhyungPark1821@users.noreply.github.com>
2023-01-27 18:47:51 -0700

    Arduino to Arduino i2c communication

--- Slave_to_Master/Slave_to_Master.ino
+++ Slave_to_Master/Slave_to_Master.ino
@@ -3,22 +3,23 @@
 String incomingData;
 
 void setup() {
+  Serial.begin(9600);
   Wire.begin(8); // join i2c bus with address 8
 }
 
 void loop() {
   // do something here
   Wire.onReceive(receiveEvent); // register event
+  delay(1000);
 }
 
 // function that executes whenever data is received from master
 // this function is registered as an event, see setup()
 void receiveEvent() {
+  incomingData = "";
   while (Wire.available()) {
     char c = Wire.read();
     incomingData += c;
   }
   Serial.println(incomingData);
 }
-
-//Common ground is necessary!!!
