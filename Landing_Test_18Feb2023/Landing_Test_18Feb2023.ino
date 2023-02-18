b5c284df57e4740b637a58832424596b7d375d89
JunhyungPark1821 <50926511+JunhyungPark1821@users.noreply.github.com>
2023-02-17 22:03:48 -0700

    Found that payload teensy's spi does not work

--- Landing_Test_18Feb2023/Landing_Test_18Feb2023.ino
+++ Landing_Test_18Feb2023/Landing_Test_18Feb2023.ino
@@ -1,9 +1,10 @@
 #include <Wire.h>
-
-//David's IMU library----------------------------------
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BNO055.h>
 #include <math.h>
+
+//Gyroscope library
+#include <Adafruit_ICM20X.h>
+#include <Adafruit_ICM20948.h>
+#include <Adafruit_Sensor.h>
 
 //Altimeter library-------------------------------------
 #include <SparkFunMPL3115A2.h>
@@ -12,10 +13,19 @@
 #include <SPI.h>
 #include <RH_RF95.h>
 
+//Gyroscope object instance
+Adafruit_ICM20948 icm;
+
 //Altimeter object instance
 MPL3115A2 alt;
 double currAlt;
 double initialAlt;
+
+// For SPI mode, we need a CS/SCK/MOSI/MISO pins
+#define ICM_CS 10 // CS
+#define ICM_SCK 13  //SCL
+#define ICM_MISO 12 //SDO
+#define ICM_MOSI 11 //SDA
 
 //Transmission pinout-----------------------
 #define RFM95_CS 0
@@ -39,9 +49,7 @@ RH_RF95 rf95(RFM95_CS, RFM95_INT);
 
 char radiopacket[20] = "Landed";
 
-// David' IMU 
-#define BNO055_SAMPLERATE_DELAY_MS (1000)
-Adafruit_BNO055 myIMU = Adafruit_BNO055(1, 0x28);
+// IMU setting ---------------------------------------------
 bool settled = false;
 bool launched = false;
 float g = 9.81;
@@ -58,32 +66,32 @@ void setup() {
   Serial.println("Altimeter found!");
 
   //-------------------Transmission setup--------------------------------------------------------------
-  pinMode(RFM95_RST, OUTPUT);
-  digitalWrite(RFM95_RST, HIGH);
-
-  // manual reset
-  digitalWrite(RFM95_RST, LOW);
-  delay(10);
-  digitalWrite(RFM95_RST, HIGH);
-  delay(10);
-
-  while (!rf95.init()) {
-    Serial.println("LoRa radio init failed");
-    Serial.println("Uncomment '#define SERIAL_DEBUG' in RH_RF95.cpp for detailed debug info");
-    while (1);
-  }
-  
-  if (!rf95.setFrequency(RF95_FREQ)) {
-    Serial.println("setFrequency failed");
-    while (1);
-  }
-  
-  rf95.setTxPower(23, false);
-
-  char testPacket[4] = "test";
-  
-  rf95.send((uint8_t *)testPacket, 4);
-  rf95.waitPacketSent();
+//  pinMode(RFM95_RST, OUTPUT);
+//  digitalWrite(RFM95_RST, HIGH);
+//
+//  // manual reset
+//  digitalWrite(RFM95_RST, LOW);
+//  delay(10);
+//  digitalWrite(RFM95_RST, HIGH);
+//  delay(10);
+//
+//  while (!rf95.init()) {
+//    Serial.println("LoRa radio init failed");
+//    Serial.println("Uncomment '#define SERIAL_DEBUG' in RH_RF95.cpp for detailed debug info");
+//    while (1);
+//  }
+//  
+//  if (!rf95.setFrequency(RF95_FREQ)) {
+//    Serial.println("setFrequency failed");
+//    while (1);
+//  }
+//  
+//  rf95.setTxPower(23, false);
+//
+//  char testPacket[4] = "test";
+//  
+//  rf95.send((uint8_t *)testPacket, 4);
+//  rf95.waitPacketSent();
   
 
   //-------------------Initiate the configuration process-------------------------------------------------
@@ -95,6 +103,71 @@ void setup() {
   alt.enableEventFlags(); // Enable all three pressure and temp event flags 
 
   initialAlt = alt.readAltitude();
+  Serial.println(initialAlt);
+
+   // Initializes IMU sensor
+  if(!icm.begin_SPI(ICM_CS)) {
+      Serial.println("Failed to find gyroscope");
+      while (1) {
+        icm.begin_SPI(ICM_CS);
+      }
+  }
+  Serial.println("Gyroscope Found!");
+
+  // Configures IMU
+  icm.setAccelRange(ICM20948_ACCEL_RANGE_8_G);
+  Serial.print("Accelerometer range set to: ");
+  switch (icm.getAccelRange()) {
+    case ICM20948_ACCEL_RANGE_2_G:
+      Serial.println("+-2G");
+      break;
+    case ICM20948_ACCEL_RANGE_4_G:
+      Serial.println("+-4G");
+      break;
+    case ICM20948_ACCEL_RANGE_8_G:
+      Serial.println("+-8G");
+      break;
+    case ICM20948_ACCEL_RANGE_16_G:
+      Serial.println("+-16G");
+      break;
+  }
+
+  icm.setGyroRange(ICM20948_GYRO_RANGE_250_DPS);
+  Serial.print("Gyro range set to: ");
+  switch (icm.getGyroRange()) {
+    case ICM20948_GYRO_RANGE_250_DPS:
+      Serial.println("250 degrees/s");
+      break;
+    case ICM20948_GYRO_RANGE_500_DPS:
+      Serial.println("500 degrees/s");
+      break;
+    case ICM20948_GYRO_RANGE_1000_DPS:
+      Serial.println("1000 degrees/s");
+      break;
+    case ICM20948_GYRO_RANGE_2000_DPS:
+      Serial.println("2000 degrees/s");
+      break;
+  }
+
+//  icm.setAccelRateDivisor(1);
+  uint16_t accel_divisor = icm.getAccelRateDivisor();
+  float accel_rate = 1125 / (1.0 + accel_divisor);
+
+  Serial.print("Accelerometer data rate divisor set to: ");
+  Serial.println(accel_divisor);
+  Serial.print("Accelerometer data rate (Hz) is approximately: ");
+  Serial.println(accel_rate);
+
+//  icm.setGyroRateDivisor(1);
+  uint8_t gyro_divisor = icm.getGyroRateDivisor();
+  float gyro_rate = 1100 / (1.0 + gyro_divisor);
+
+  Serial.print("Gyro data rate divisor set to: ");
+  Serial.println(gyro_divisor);
+  Serial.print("Gyro data rate (Hz) is approximately: ");
+  Serial.println(gyro_rate);
+
+  icm.enableAccelDLPF(true, ICM20X_ACCEL_FREQ_11_5_HZ);
 
   //----------------------SD Card initialization-----------------------------------------------------------
   // Initializes SD card reader and csv file
@@ -126,18 +199,11 @@ void setup() {
   // Labels columns of csv file
   myFlightData.print("Time(ms),Accel(X)(m/s^2),Accel(Y)(m/s^2),Accel(Z)(m/s^2),Gyro(X)(rad/s),Gyro(Y)(rad/s),Gyro(Z)(rad/s),Altitude(m)\n");
 
-  //David's IMU setup ----------------------------------------------------------------------------------------------------------------------
-  myIMU.begin();
-  delay(1000);
-  //int8_t temp=myIMU.getTemp();
-  myIMU.setExtCrystalUse(true);
-  startTime=millis();  
-
   //------------------------Buzzer setup------------------------------------------------------------------------------------------------
   pinMode(6, OUTPUT); // Set buzzer - pin 6 as an output
 
   tone(6, 1000); // Send 1KHz sound signal...
-  delay(1000);        // ...for 1 sec
+  delay(2000);        // ...for 1 sec
   noTone(6);     // Stop sound...
 }
  
@@ -145,18 +211,22 @@ void loop() {
   //Altimeter reading
   currAlt = alt.readAltitude();
 
+  /* Gets a new normalized sensor event from IMU */
+  sensors_event_t accel;
+  sensors_event_t gyro;
+  sensors_event_t mag;
+  sensors_event_t temp;
   
-  //uint8_t system, gyro, accel, mg = 0;
-  //myIMU.getCalibration(&system, &gyro, &accel, &mg);
-  imu::Vector<3> acc = myIMU.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
-  imu::Vector<3> gyro = myIMU.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
-  myFlightData.println(String(millis()) + "," + String(acc.x()) + "," + String(acc.y()) + "," + String(acc.z())) + "," + String(gyro.x()) + "," + String(gyro.y()) + "," + String(gyro.z())+","+String(alt.readAltitude());
-  double magnitude = sqrt(pow(acc.x(),2) + pow(acc.y(),2) + pow(acc.z(),2));
-//  Serial.print("Mag: ");
-//  Serial.println(magnitude);
+  icm.getEvent(&accel, &gyro, &temp, &mag);
+  
+  myFlightData.println(String(millis()) + "," + String(accel.acceleration.x) + "," + String(accel.acceleration.y) + "," + String(accel.acceleration.z) + "," + String(gyro.gyro.x) + "," + String(gyro.gyro.y) + "," + String(gyro.gyro.z) + "," + String(alt.readAltitude()));
+  Serial.println(accel.acceleration.x);
+  double magnitude = sqrt(pow(accel.acceleration.x,2) + pow(accel.acceleration.y,2) + pow(accel.acceleration.z,2));
+  Serial.print("Mag: ");
+  Serial.println(magnitude);
   if(magnitude >= (g*launchForceMultiplier)){
     launched = true;
-//    Serial.println("launched");
+    Serial.println("launched");
   }
   if(launched){
     if(millis()-startTime >= 5000){
@@ -196,5 +266,5 @@ void loop() {
       }   
     }
   }
-  delay(BNO055_SAMPLERATE_DELAY_MS);
+  delay(500);
 }
