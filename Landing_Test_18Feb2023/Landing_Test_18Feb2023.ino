f3d6b61423b8e05401a42f10df86adddc77dce84
David Ke <93749669+DavidKe410@users.noreply.github.com>
2023-02-27 18:16:15 -0700

    Update Landing_Test_18Feb2023.ino

--- Landing_Test_18Feb2023/Landing_Test_18Feb2023.ino
+++ Landing_Test_18Feb2023/Landing_Test_18Feb2023.ino
@@ -19,17 +19,30 @@ const int chipSelect = BUILTIN_SDCARD;
 boolean fileNotCreated = true;
 int fileNum = 1;
 
-// David' IMU 
+// David' IMU-------------------------------------------------------
+/* The #define: Millisecond delay before getting data, aka how fast void loop() runs
+ *  The Adafruit: Initializing sensor with IMU Address for I2C
+ *  The 2 Booleans: for when we have sensed the rocket has launched or settled (not moving on the ground)
+ */
 #define BNO055_SAMPLERATE_DELAY_MS (1000)
-Adafruit_BNO055 myIMU = Adafruit_BNO055(1, 0x28);
+Adafruit_BNO055 BNO = Adafruit_BNO055(1, 0x28);
 bool settled = false;
 bool launched = false;
+/* Constants: g is gravity (m/s^2)
+ *  launchForceMultiplier changes how large of a magnitude of acceleration must be felt to trigger launched (higher means more force is needed to trigger launch)
+ *  margin is the percent of gravity which is +/- to g to make a range of acceptable values for whther the rocket has settled, thus should be close to 9.81 
+ *  valuesRecorded is the size of the array of recorded accelerometer and altimeter values 
+ *  pastAccelerations[valuesRecorded] is an array of the most recent magnitude-of-acceleration values
+ *  accelLandingDelay is when after launching, how much time is between each read of acceleration
+ *  recordedTime will be initialized to record a certain time which will be used to  see whether a certain amount of time has passed.
+*/
 float g = 9.81;
 float launchForceMultiplier = 3;
 float margin = 0.05;
 const int valuesRecorded = 3;
 double pastAccelerations[valuesRecorded] = {-1.0};
-unsigned long startTime;
+int accelLandingDelay = 5000
+unsigned long recordedTime;
 
 
 // Altitude past data
@@ -78,13 +91,13 @@ void setup() {
 //  Serial.println("Opened flight.csv");
 
   //David's IMU setup ----------------------------------------------------------------------------------------------------------------------
-  if (!myIMU.begin()) {
+  if (!BNO.begin()) {
 //    Serial.println("Imu did not initiate");
     while (1);
   }
   delay(1000);
-  myIMU.setExtCrystalUse(true);
-  startTime=millis();  
+  BNO.setExtCrystalUse(true);
+  recordedTime=millis();
 
   //------------------------Buzzer setup------------------------------------------------------------------------------------------------
   pinMode(6, OUTPUT); // Set buzzer - pin 6 as an output
@@ -97,37 +110,48 @@ void setup() {
 void loop() {
   //Altimeter reading
   currAlt = alt.readAltitude();
-//  Serial.println(currAlt);
-  imu::Vector<3> acc = myIMU.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
-  imu::Vector<3> gyro = myIMU.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
-  double magnitude = sqrt(pow(acc.x(),2) + pow(acc.y(),2) + pow(acc.z(),2));
   
-//  Serial.print("Mag: ");
-//  Serial.println(magnitude);
+  //Reading accelerometer and gyroscopic data
+  imu::Vector<3> acc = BNO.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
+  imu::Vector<3> gyro = BNO.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
+  
+  //Disregarding direction of acceleration to find the total magnitude
+  double magnitudeAccel = sqrt(pow(acc.x(),2) + pow(acc.y(),2) + pow(acc.z(),2));
 
+  /* To keep the data in sequencial order and to standardize a process of recording
+   * Each value in the array will be shifted to the left and the new value added on to the last index
+   */
   for(int i =0; i<valuesRecorded-1; i++){
     pastAltitudes[i] = pastAltitudes[i+1];
   }
   pastAltitudes[valuesRecorded-1] = currAlt;
-  
-//  if ( (magnitude >= (g*launchForceMultiplier)) || ((pastAltitudes[2]-pastAltitudes[0])>25)) {
-  if ((magnitude >= (g*launchForceMultiplier)) || ((pastAltitudes[2]-pastAltitudes[0]) > 25)) {
-    myFlightData.println(String(millis()) + "," + "Magintude" + "," + String(magnitude) + "," + "Altitudes" + "," + String(pastAltitudes[0]) + "," + String(pastAltitudes[2]));
+
+  // Reducing false positives by creating redundency by comparing the acceleration at any instant and the change in altitude s
+  if (((magnitudeAccel >= (g*launchForceMultiplier)) || ((pastAltitudes[2]-pastAltitudes[0]) > 25)) && !launched) {
+    myFlightData.println(String(millis()) + "," + "Magintude" + "," + String(magnitudeAccel) + "," + "Altitudes" + "," + String(pastAltitudes[0]) + "," + String(pastAltitudes[2]));
     // Labels columns of csv file
-    myFlightData.println("Time(ms),Accel(X)(m/s^2),Accel(Y)(m/s^2),Accel(Z)(m/s^2),Gyro(X)(rad/s),Gyro(Y)(rad/s),Gyro(Z)(rad/s),Altitude(m)\n");
+    myFlightData.println("Time(ms),Magnitude of Accel,Accel(X)(m/s^2),Accel(Y)(m/s^2),Accel(Z)(m/s^2),Gyro(X)(rad/s),Gyro(Y)(rad/s),Gyro(Z)(rad/s),Altitude(m)\n");
     launched = true;
-//    Serial.println("launched");
   }
   
   if(launched){
-    myFlightData.println(String(millis()) + "," + String(acc.x()) + "," + String(acc.y()) + "," + String(acc.z()) + "," + String(gyro.x()) + "," + String(gyro.y()) + "," + String(gyro.z())+","+String(currAlt));
-    if(millis()-startTime >= 5000){
+    myFlightData.println(String(millis()) + "," + magnitudeAccel + "," + String(acc.x()) + "," + String(acc.y()) + "," + String(acc.z()) + "," + String(gyro.x()) + "," + String(gyro.y()) + "," + String(gyro.z())+","+String(currAlt));
+    if(millis()-recordedTime >= accelLandingDelay){
+      /* Similar to the loop used for the altimeter data: standardize process of recording
+       * Each value in array shifted to the left and new value added on to the last index
+       */
       for(int i =0; i<valuesRecorded-1; i++){
         pastAccelerations[i] = pastAccelerations[i+1];
       }
-      pastAccelerations[valuesRecorded-1] = magnitude;
-      startTime = millis();
+      pastAccelerations[valuesRecorded-1] = magnitudeAccel;
+      recordedTime = millis();
     }
+    // To test whether we have actually settled on the ground we run through all the recorded data
+    // Nested if statement logic same as &&
+
+
+
+    // ======================!!!!!!!Launch was fine, landing was wrong, probably change accel to jerk and change altitude range so it doesn't need to be =2!!!!!!!===================================
     for(int i=0; i<valuesRecorded; i++){   
       if((pastAccelerations[i] > ((1+margin)*g)) || (pastAccelerations[i] < ((1-margin)*g))) {
         if (((pastAltitudes[2]-pastAltitudes[0])>2) || ((pastAltitudes[2]-pastAltitudes[0])<2)) {
@@ -142,12 +166,12 @@ void loop() {
         myFlightData.close();
       }
     }
-    if(settled){
-      while(1) {
-        //-----------------------Beeping sound----------------------------------------
-        tone(6, 1000); // Send 1KHz sound signal...
-        delay(1000);        // ...for 1 sec
-        noTone(6);     // Stop sound...
+  if(settled){
+    while(1) {
+      //-----------------------Beeping sound----------------------------------------
+      tone(6, 1000); // Send 1KHz sound signal...
+      delay(1000);        // ...for 1 sec
+      noTone(6);     // Stop sound...
       }   
     }
   }
