84eca5d9b835055627dd6724aa25bd3932658846
JunhyungPark1821 <50926511+JunhyungPark1821@users.noreply.github.com>
2023-03-16 23:41:33 -0600

    Organized files and i2c comm from arduino rbpi

---
+++ Arduino/i2c_with_rbpi/i2c_with_rbpi.ino
@@ -0,0 +1,40 @@
+#include <Wire.h>
+
+#define SLAVE_ADDRESS 0x08
+
+boolean landed = true;
+boolean sent = false;
+boolean received = false;
+
+char tasks[100];
+
+int charIndex = 0;
+
+void setup() {
+  Wire.begin(SLAVE_ADDRESS);        // join i2c bus (address optional for master)
+  while(Wire.available()) {
+    Wire.write(1);
+    delay(1000);
+  }
+  Serial.begin(9600);  // start serial for output
+}
+
+void loop() {
+  while (Wire.available() && landed && !sent) { // peripheral may send less than requested
+    Wire.write(1);
+    delay(1000);
+    sent = true;
+  }
+  
+  while (Wire.available() && !received) {
+    char c = Wire.read(); // receive a byte as character
+    tasks[charIndex] = c;
+    charIndex++;
+    if (c == '6') {
+      received = true;
+      for (int i = 0; i<charIndex; i++) {
+        Serial.print(tasks[i]);
+      }
+    }
+  }
+}
